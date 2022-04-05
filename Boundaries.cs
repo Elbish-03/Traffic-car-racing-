a2ef7c5cc5a6aacdd69273d5fe7a2198401e3954
Elbish-03 <78097812+Elbish-03@users.noreply.github.com>
2022-04-05 13:17:57 +0200

    Add files via upload

---
+++ Boundaries.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Boundaries : MonoBehaviour
+{
+    private Vector2 screenBounds;
+    private float objectWidth;
+    private float objectHeight;
+    void Start()
+    {
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        objectWidth = transform.GetComponent<SpriteRenderer>().bounds.size.x / 2;
+        objectHeight = transform.GetComponent<SpriteRenderer>().bounds.size.y / 2;
+    }
+
+
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 viewPos = transform.position;
+        viewPos.x = Mathf.Clamp(viewPos.x, screenBounds.x, screenBounds.x * 5 - objectWidth);
+        viewPos.y = Mathf.Clamp(viewPos.y, screenBounds.y, screenBounds.y * -5 - objectHeight);
+        transform.position = viewPos;
+    }
+}
